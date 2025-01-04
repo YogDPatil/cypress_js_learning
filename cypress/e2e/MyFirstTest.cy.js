1d47b7f7821470bb3d4f977957d299c5f681cef8
YogDPatil <ydpatil.it@gmail.com>
2025-01-04 16:51:57 +0530

    assertion code added

--- cypress/e2e/MyFirstTest.cy.js
+++ cypress/e2e/MyFirstTest.cy.js
@@ -1,6 +1,8 @@
 
 describe('my first suit', function(){
     it('my first test case', function(){
-        cy.visit("https://accounts.staging.besimplified.net/auth/sign-in?page=yes")
+        cy.visit("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
+        // verify the title of app
+        cy.title().should('eq', 'OrangeHRM')
     })
 })
